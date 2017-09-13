64279301de5014cdb02068a9b4aa2d8c1920f8c5
Helene <lenagritsuk@inbox.ru>
2017-09-13 19:47:14 +0300

    firstcommit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	require_once('templates/top.php');
+?>
+	   <div class="zag">Каталог цветов</div>
+	   <div class="block_for_pics">
+	    <div><img src="media/img/pic1.jpg" /><br />asdf</div>
+	    <img src="media/img/pic2.jpg" />
+	    <img src="media/img/pic3.jpg" />
+	    <img src="media/img/pic1.jpg" />
+	    <img src="media/img/pic2.jpg" />
+	    <img src="media/img/pic3.jpg" />
+	   </div>
+<?php
+	require_once('templates/bottom.php');
+?>
---
+++ templates/top.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Структура документа HTML5</title>
+  <meta charset="utf-8">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link type="text/css" href="media/style.css" rel="stylesheet" />  
+</head>
+<body>
+ <div class="mainblock">
+ <div class="top" align="center">
+	  <nav class="topmenu">
+		<a href="#">О компании</a> 
+		<a href="#">Новости цветов</a> 
+		<a href="#">Доставка цветов</a> 
+		<a href="#">Статьи о цветах</a> 
+	  </nav>
+	  <header class="topmenu" id="bighead">
+		<a href="#" class="head_left"><img src="media/img/buket_left.png" ></a> 
+		<a href="#" class="head_middle"><img src="media/img/logo.png" align="middle" border=0></a> 	
+		<a href="#" class="head_right"><img src="media/img/buket_right.png" ></a>	
+	  </header>
+	  <div class="block_for_text">
